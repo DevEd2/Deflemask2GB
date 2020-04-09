91ccdbc6635deb4943f2d5a22350be76c30c44e9
Messmer <messmerd97@gmail.com>
2020-04-09 12:04:52 -0400

    Added rgbfix to test scripts

--- SongInserter/tests.sh
+++ SongInserter/tests.sh
@@ -1,30 +1,60 @@
 ./SongInserter ./DemoSongs/asterix_challenge_level.gb ./DemoSongs/asterix_challenge_level.gbs 
+rgbfix -v -p 255 ./DemoSongs/asterix_challenge_level.gb
 ./SongInserter ./DemoSongs/asterix_stage.gb ./DemoSongs/asterix_stage.gbs 
+rgbfix -v -p 255 ./DemoSongs/asterix_stage.gb
 ./SongInserter ./DemoSongs/asterix_stage1.gb ./DemoSongs/asterix_stage1.gbs 
+rgbfix -v -p 255 ./DemoSongs/asterix_stage1.gb
 ./SongInserter ./DemoSongs/asterix_stage2.gb ./DemoSongs/asterix_stage2.gbs 
+rgbfix -v -p 255 ./DemoSongs/asterix_stage2.gb
 ./SongInserter ./DemoSongs/bergentruckung.gb ./DemoSongs/bergentruckung.gbs 
+rgbfix -v -p 255 ./DemoSongs/bergentruckung.gb
 ./SongInserter ./DemoSongs/crystal_caverns.gb ./DemoSongs/crystal_caverns.gbs 
+rgbfix -v -p 255 ./DemoSongs/crystal_caverns.gb
 ./SongInserter ./DemoSongs/darkman.gb ./DemoSongs/darkman.gbs 
+rgbfix -v -p 255 ./DemoSongs/darkman.gb
 ./SongInserter ./DemoSongs/die_maus.gb ./DemoSongs/die_maus.gbs 
+rgbfix -v -p 255 ./DemoSongs/die_maus.gb
 ./SongInserter ./DemoSongs/dr_wright.gb ./DemoSongs/dr_wright.gbs 
+rgbfix -v -p 255 ./DemoSongs/dr_wright.gb
 ./SongInserter ./DemoSongs/jingle_bells.gb ./DemoSongs/jingle_bells.gbs 
+rgbfix -v -p 255 ./DemoSongs/jingle_bells.gb
 ./SongInserter ./DemoSongs/jurassic_park_bonus.gb ./DemoSongs/jurassic_park_bonus.gbs 
+rgbfix -v -p 255 ./DemoSongs/jurassic_park_bonus.gb
 ./SongInserter ./DemoSongs/jurassic_park_lvl2.gb ./DemoSongs/jurassic_park_lvl2.gbs 
+rgbfix -v -p 255 ./DemoSongs/jurassic_park_lvl2.gb
 ./SongInserter ./DemoSongs/kraken_of_the_sea.gb ./DemoSongs/kraken_of_the_sea.gbs 
+rgbfix -v -p 255 ./DemoSongs/kraken_of_the_sea.gb
 ./SongInserter ./DemoSongs/ltcc_zoo.gb ./DemoSongs/ltcc_zoo.gbs 
+rgbfix -v -p 255 ./DemoSongs/ltcc_zoo.gb
 ./SongInserter ./DemoSongs/megaman_v_stageselect.gb ./DemoSongs/megaman_v_stageselect.gbs 
+rgbfix -v -p 255 ./DemoSongs/megaman_v_stageselect.gb
 ./SongInserter ./DemoSongs/mohenjodaro_usas.gb ./DemoSongs/mohenjodaro_usas.gbs 
+rgbfix -v -p 255 ./DemoSongs/mohenjodaro_usas.gb
 ./SongInserter ./DemoSongs/monster_max.gb ./DemoSongs/monster_max.gbs 
+rgbfix -v -p 255 ./DemoSongs/monster_max.gb
 ./SongInserter ./DemoSongs/ottifanten_woods.gb ./DemoSongs/ottifanten_woods.gbs 
+rgbfix -v -p 255 ./DemoSongs/ottifanten_woods.gb
 ./SongInserter ./DemoSongs/pokemon_route_01.gb ./DemoSongs/pokemon_route_01.gbs 
+rgbfix -v -p 255 ./DemoSongs/pokemon_route_01.gb
 ./SongInserter ./DemoSongs/shop_theme.gb ./DemoSongs/shop_theme.gbs 
+rgbfix -v -p 255 ./DemoSongs/shop_theme.gb
 ./SongInserter ./DemoSongs/smurf_nightmare_well.gb ./DemoSongs/smurf_nightmare_well.gbs 
+rgbfix -v -p 255 ./DemoSongs/smurf_nightmare_well.gb
 ./SongInserter ./DemoSongs/soldier_blade_op1.gb ./DemoSongs/soldier_blade_op1.gbs 
+rgbfix -v -p 255 ./DemoSongs/soldier_blade_op1.gb
 ./SongInserter ./DemoSongs/super_star_soldier_area1.gb ./DemoSongs/super_star_soldier_area1.gbs 
+rgbfix -v -p 255 ./DemoSongs/super_star_soldier_area1.gb
 ./SongInserter ./DemoSongs/super_star_soldier_area2.gb ./DemoSongs/super_star_soldier_area2.gbs 
+rgbfix -v -p 255 ./DemoSongs/super_star_soldier_area2.gb
 ./SongInserter ./DemoSongs/tomjerry_level3.gb ./DemoSongs/tomjerry_level3.gbs 
+rgbfix -v -p 255 ./DemoSongs/tomjerry_level3.gb
 ./SongInserter ./DemoSongs/towntheme.gb ./DemoSongs/towntheme.gbs 
+rgbfix -v -p 255 ./DemoSongs/towntheme.gb
 ./SongInserter ./DemoSongs/trouballs_title.gb ./DemoSongs/trouballs_title.gbs 
+rgbfix -v -p 255 ./DemoSongs/trouballs_title.gb
 ./SongInserter ./DemoSongs/undertale_death.gb ./DemoSongs/undertale_death.gbs 
+rgbfix -v -p 255 ./DemoSongs/undertale_death.gb
 ./SongInserter ./DemoSongs/usas_jubaruins.gb ./DemoSongs/usas_jubaruins.gbs 
+rgbfix -v -p 255 ./DemoSongs/usas_jubaruins.gb
 ./SongInserter ./DemoSongs/v_rally_carselect.gb ./DemoSongs/v_rally_carselect.gbs 
+rgbfix -v -p 255 ./DemoSongs/v_rally_carselect.gb
