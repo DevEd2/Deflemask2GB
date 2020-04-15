4b5b0b30d61e7bdc093fd34dfc569933cd32be43
DevEd2 <deved8@gmail.com>
2020-04-15 11:44:33 -0400

    manual push because git is stupid

---
+++ SongInserter/build.sh
@@ -0,0 +1,1 @@
+gcc ./SongInserter.c -o ./SongInserter  
---
+++ SongInserter/tests.bat
@@ -0,0 +1,60 @@
+.\SongInserter.exe .\DemoSongs\asterix_challenge_level.gb .\DemoSongs\asterix_challenge_level.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\asterix_challenge_level.gb
+.\SongInserter.exe .\DemoSongs\asterix_stage.gb .\DemoSongs\asterix_stage.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\asterix_stage.gb
+.\SongInserter.exe .\DemoSongs\asterix_stage1.gb .\DemoSongs\asterix_stage1.gbs
+..\bin\rgbfix -v -p 255 .\DemoSongs\asterix_stage1.gb
+.\SongInserter.exe .\DemoSongs\asterix_stage2.gb .\DemoSongs\asterix_stage2.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\asterix_stage2.gb
+.\SongInserter.exe .\DemoSongs\bergentruckung.gb .\DemoSongs\bergentruckung.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\bergentruckung.gb
+.\SongInserter.exe .\DemoSongs\crystal_caverns.gb .\DemoSongs\crystal_caverns.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\crystal_caverns.gb
+.\SongInserter.exe .\DemoSongs\darkman.gb .\DemoSongs\darkman.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\darkman.gb
+.\SongInserter.exe .\DemoSongs\die_maus.gb .\DemoSongs\die_maus.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\die_maus.gb
+.\SongInserter.exe .\DemoSongs\dr_wright.gb .\DemoSongs\dr_wright.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\dr_wright.gb
+.\SongInserter.exe .\DemoSongs\jingle_bells.gb .\DemoSongs\jingle_bells.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\jingle_bells.gb
+.\SongInserter.exe .\DemoSongs\jurassic_park_bonus.gb .\DemoSongs\jurassic_park_bonus.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\jurassic_park_bonus.gb
+.\SongInserter.exe .\DemoSongs\jurassic_park_lvl2.gb .\DemoSongs\jurassic_park_lvl2.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\jurassic_park_lvl2.gb
+.\SongInserter.exe .\DemoSongs\kraken_of_the_sea.gb .\DemoSongs\kraken_of_the_sea.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\kraken_of_the_sea.gb
+.\SongInserter.exe .\DemoSongs\ltcc_zoo.gb .\DemoSongs\ltcc_zoo.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\ltcc_zoo.gb
+.\SongInserter.exe .\DemoSongs\megaman_v_stageselect.gb .\DemoSongs\megaman_v_stageselect.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\megaman_v_stageselect.gb
+.\SongInserter.exe .\DemoSongs\mohenjodaro_usas.gb .\DemoSongs\mohenjodaro_usas.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\mohenjodaro_usas.gb
+.\SongInserter.exe .\DemoSongs\monster_max.gb .\DemoSongs\monster_max.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\monster_max.gb
+.\SongInserter.exe .\DemoSongs\ottifanten_woods.gb .\DemoSongs\ottifanten_woods.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\ottifanten_woods.gb
+.\SongInserter.exe .\DemoSongs\pokemon_route_01.gb .\DemoSongs\pokemon_route_01.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\pokemon_route_01.gb
+.\SongInserter.exe .\DemoSongs\shop_theme.gb .\DemoSongs\shop_theme.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\shop_theme.gb
+.\SongInserter.exe .\DemoSongs\smurf_nightmare_well.gb .\DemoSongs\smurf_nightmare_well.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\smurf_nightmare_well.gb
+.\SongInserter.exe .\DemoSongs\soldier_blade_op1.gb .\DemoSongs\soldier_blade_op1.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\soldier_blade_op1.gb
+.\SongInserter.exe .\DemoSongs\super_star_soldier_area1.gb .\DemoSongs\super_star_soldier_area1.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\super_star_soldier_area1.gb
+.\SongInserter.exe .\DemoSongs\super_star_soldier_area2.gb .\DemoSongs\super_star_soldier_area2.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\super_star_soldier_area2.gb
+.\SongInserter.exe .\DemoSongs\tomjerry_level3.gb .\DemoSongs\tomjerry_level3.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\tomjerry_level3.gb
+.\SongInserter.exe .\DemoSongs\towntheme.gb .\DemoSongs\towntheme.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\towntheme.gb
+.\SongInserter.exe .\DemoSongs\trouballs_title.gb .\DemoSongs\trouballs_title.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\trouballs_title.gb
+.\SongInserter.exe .\DemoSongs\undertale_death.gb .\DemoSongs\undertale_death.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\undertale_death.gb
+.\SongInserter.exe .\DemoSongs\usas_jubaruins.gb .\DemoSongs\usas_jubaruins.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\usas_jubaruins.gb
+.\SongInserter.exe .\DemoSongs\v_rally_carselect.gb .\DemoSongs\v_rally_carselect.gbs 
+..\bin\rgbfix -v -p 255 .\DemoSongs\v_rally_carselect.gb
